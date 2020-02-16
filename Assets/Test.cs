678d2fb9437b77f6eb34ebff005f6de88cc668ec
kotaromurata <mtk130127@gmail.com>
2020-02-16 16:32:43 +0900

    ScriptTestプロジェクト　C#スクリプト
    LESSON4の課題

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = new int[5];
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        //配列を順番に表示
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        //配列を逆順に表示
+        for (int i = 5; i > 0; i--)
+        {
+            Debug.Log(array[i - 1]);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    private int mp = 53;
+
+    public void Magic() {
+        mp -= 5;
+        if (mp < 5) {
+            Debug.Log("MPが足りません");
+        } else {
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+    }
+}
+
+public class Test2 : MonoBehaviour
+{
+
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        for (int i = 0; i < 10; i++)
+            lastboss.Magic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
