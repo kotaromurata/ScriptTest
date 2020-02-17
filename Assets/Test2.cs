eb3bd6027dc6ed3a3706a08973ad115aafc18158
kotaromurata <mtk130127@gmail.com>
2020-02-17 21:16:08 +0900

    LESSON4　発展課題
    else内にmp -= 5

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -6,23 +6,25 @@ public class Boss
     private int mp = 53;
 
     public void Magic() { 
-        mp -= 5;
-        if (mp <= 0) {
-            Debug.Log("MPが足りません");
-        } else {
-            Debug.Log("魔法攻撃をした。残りMPは" + mp);
-        }
+            if (mp < 5)
+            {
+                Debug.Log("MPが足りません");
+            }
+            else
+            {
+                mp -= 5;
+                Debug.Log("魔法攻撃をした。残りMPは" + mp);
+            }
     }
 }
 
 public class Test2 : MonoBehaviour
 {
-
     void Start()
     {
         Boss lastboss = new Boss();
 
-        for (int i = 0; i < 11 ; i++)
+        for (int i = 0; i < 12 ; i++)
             lastboss.Magic();
     }
 
